1a80914bebc046da057082fec5f7dbb404003760
Simon Dietz <simon.dietz@plantwatch.de>
2026-08-28 16:44:11 +0200

    prepare for adding tests

--- src/main/java/com/oberamsystems/ai/pcmanager/PcmanagerApplication.java
+++ src/main/java/com/oberamsystems/ai/pcmanager/PcmanagerApplication.java
@@ -11,24 +11,26 @@ import com.oberamsystems.ai.pcmanager.repository.ComponentTypeRepository;
 @SpringBootApplication
 public class PcmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcmanagerApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(PcmanagerApplication.class, args);
+        }
 
-	@Bean
-	public CommandLineRunner initProcs(ComponentTypeRepository repository) {
-		return (args) -> {
-			/*
-			 * repository.save(new ComponentType("CPU"));
-			 * repository.save(new ComponentType("Mainboard"));
-			 * repository.save(new ComponentType("RAM"));
-			 * repository.save(new ComponentType("PSU"));
-			 * repository.save(new ComponentType("SSD"));
-			 * repository.save(new ComponentType("HDD"));
-			 * repository.save(new ComponentType("Case"));
-			 * repository.save(new ComponentType("other"));
-			 */
-		};
-	}
+        /*
+         * @Bean
+         * public CommandLineRunner initProcs(ComponentTypeRepository repository) {
+         * return (args) -> {
+         * 
+         * repository.save(new ComponentType("CPU"));
+         * repository.save(new ComponentType("Mainboard"));
+         * repository.save(new ComponentType("RAM"));
+         * repository.save(new ComponentType("PSU"));
+         * repository.save(new ComponentType("SSD"));
+         * repository.save(new ComponentType("HDD"));
+         * repository.save(new ComponentType("Case"));
+         * repository.save(new ComponentType("other"));
+         * 
+         * };
+         * }
+         */
 
 }
